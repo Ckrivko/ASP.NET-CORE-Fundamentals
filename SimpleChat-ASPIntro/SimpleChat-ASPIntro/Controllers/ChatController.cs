244612da7d77d5cd348924cc46f0ade5f5ec6582
Ckrivko <76474570+Ckrivko@users.noreply.github.com>
2023-06-13 15:40:42 +0300

    Add files via upload

---
+++ SimpleChat-ASPIntro/SimpleChat-ASPIntro/Controllers/ChatController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleChat_ASPIntro.Models.Message;
+
+namespace SimpleChat_ASPIntro.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static List<KeyValuePair<string, string>> s_messages =
+             new List<KeyValuePair<string, string>>();
+
+        public IActionResult Show()
+        {
+            if (s_messages.Count < 1)
+            {
+                return View(new ChatViewModel());
+            }
+
+            var chatModel = new ChatViewModel()
+            {
+                Messages = s_messages
+                .Select(m => new MessageViewModel()
+                {
+                    Sender = m.Key,
+                    MessageText = m.Value
+                }).ToList()
+
+            };
+
+            return View(chatModel);
+        }
+
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat)
+        {
+            var newMessage = chat.CurrentMessage;
+
+            s_messages.Add(new KeyValuePair<string, string>
+                (newMessage.Sender, newMessage.MessageText));
+
+            return RedirectToAction("Show");
+        }
+    }
+}
---
+++ SimpleChat-ASPIntro/SimpleChat-ASPIntro/Models/Message/ChatViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleChat_ASPIntro.Models.Message
+{
+    public class ChatViewModel
+    {
+        public MessageViewModel CurrentMessage { get; set; } = null!;
+
+        public List<MessageViewModel> Messages { get; set; } = null!;
+
+    }
+}
