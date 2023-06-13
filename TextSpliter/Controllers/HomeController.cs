d8e48cde01e38c721ff4cb98578455af3a5b8fea
Ckrivko <76474570+Ckrivko@users.noreply.github.com>
2023-06-13 15:45:30 +0300

    Add files via upload

---
+++ TextSpliter/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TextSpliter.Models;
+
+namespace TextSpliter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(TextSplitViewModel model)
+        {
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Split(TextSplitViewModel model)
+        {
+           model.Text= model.Text.TrimEnd();
+            if (!ModelState.IsValid)
+            {                              
+                return RedirectToAction("Index", new TextSplitViewModel() 
+                {
+                Text="Aaaaaaaaaaaaaaaaaaaaaaa",
+                SplitText="Error!"
+                });
+            }
+
+            var splitTextArray = model
+                    .Text
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+
+            model.SplitText = string.Join(Environment.NewLine, splitTextArray);
+
+            return RedirectToAction("Index", model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TextSpliter/Models/TextSplitViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TextSpliter.Models
+{
+    public class TextSplitViewModel
+    {
+        [StringLength(30,MinimumLength =2)]
+        public string Text { get; set; }  = null!;
+
+
+        public string? SplitText { get; set; } 
+
+    }
+}
