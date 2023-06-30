4cf086ce04c33c6d84a3555b5415e70e657cd631
Ckrivko <76474570+Ckrivko@users.noreply.github.com>
2023-06-30 23:54:40 +0300

    Add files via upload

---
+++ Identity2023/Identity2023/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Identity2023.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Identity2023.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger,
+            RoleManager<IdentityRole> roleManager,
+            UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Privacy()
+        {
+
+            await _roleManager.CreateAsync(new IdentityRole("Admin"));
+            var user = await _userManager.FindByEmailAsync("vesich.dimitrov@abv.bg");
+
+            await _userManager.AddToRoleAsync(user, "Admin");
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
